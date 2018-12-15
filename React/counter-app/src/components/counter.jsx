964629a0cb31b9c9287e0c4781916f51636267fb
Paul Alwin <paulalwin91@gmail.com>
2018-12-15 14:50:12 -0500

    commit

--- React/counter-app/src/components/counter.jsx
+++ React/counter-app/src/components/counter.jsx
@@ -1,13 +1,64 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    value: this.props.value
+  };
+
   render() {
     return (
       <React.Fragment>
-        <h1>Hello World</h1>
-        <button>Increment</button>
+        <b>{this.props.id}</b>
+        <span className="btn btn-primary m-4">{this.formatCount()}</span>
+        <button
+          onClick={() => {
+            this.incrementCounter(2);
+          }}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={this.decrementCounter}
+          className="btn btn-secondary btn-sm m-4"
+        >
+          Decrement
+        </button>
+        <button
+          onClick={this.resetCounter}
+          className="btn btn-secondary btn-sm m-4"
+        >
+          Reset
+        </button>
+        <button
+          onClick={() => {
+            this.props.onDelete(this.props.id);
+          }}
+          className="btn btn-secondary btn-sm m-4"
+        >
+          Delete
+        </button>
       </React.Fragment>
     );
+  }
+
+  deleteCounter = () => {};
+
+  incrementCounter = i => {
+    this.props.onIncrement(this.props.id);
+  };
+
+  decrementCounter = () => {
+    this.props.onDecrement(this.props.id);
+  };
+
+  resetCounter = () => {
+    this.setState({ value: 0 });
+  };
+
+  formatCount() {
+    const { value } = this.props;
+    return value === 0 ? <h1>0</h1> : <h1>{value}</h1>;
   }
 }
 
